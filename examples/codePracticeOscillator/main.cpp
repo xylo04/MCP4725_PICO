3645b14d8a825e3dfe24affcec48020aab5743b7
Chris Keller <xylo04@gmail.com>
2025-03-09 20:01:33 -0600

    Fix up and use LED

--- examples/codePracticeOscillator/main.cpp
+++ examples/codePracticeOscillator/main.cpp
@@ -12,15 +12,17 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-#define DAC_REF_VOLTAGE 5     // Volts DAC supply-reference voltage
-#define MORSE_KEY_GPIO_PIN 21 // GPIO pin for the Morse Code key
+#define DAC_I2C_ADDRESS myDAC.MCP4725A0_Addr_A00 // MCP4725 I2C address
+#define DAC_REF_VOLTAGE 3.3   // Volts DAC supply-reference voltage
+#define MORSE_KEY_GPIO_PIN 20 // GPIO pin for the Morse Code key
+#define LED_GPIO_PIN 25       // GPIO pin for the LED
 #define I2C_BLOCK i2c0        // I2C block
 #define I2C_SDA_PIN 16        // I2C GPIO for data line
 #define I2C_SCL_PIN 17        // I2C GPIO for Clock line
-#define I2C_SPEED 3000        // I2C speed in Khz
+#define I2C_SPEED 1000        // I2C speed in Khz
 #define I2C_TIMEOUT 50000     // I2C timeout delay in uS.
 
-uint32_t sineWaveFrequency = 800;                // Frequency in Hz
+uint32_t sineWaveFrequency = 600;                // Frequency in Hz
 uint32_t periodUs = 1000000 / sineWaveFrequency; // Period in microseconds
 
 MCP4725_PICO myDAC(DAC_REF_VOLTAGE);
@@ -31,7 +33,7 @@ int main() {
   busy_wait_ms(1000);
   printf("MCP4725_PICO : Code Practice Oscillator\r\n");
 
-  if (!myDAC.begin(myDAC.MCP4725A0_Addr_A00, I2C_BLOCK, I2C_SPEED, I2C_SDA_PIN,
+  if (!myDAC.begin(DAC_I2C_ADDRESS, I2C_BLOCK, I2C_SPEED, I2C_SDA_PIN,
                    I2C_SCL_PIN, I2C_TIMEOUT)) {
     printf("MCP4725 : Failed to initialize DAC.!\r\n");
     while (1) {
@@ -43,6 +45,12 @@ int main() {
   gpio_set_dir(MORSE_KEY_GPIO_PIN, GPIO_IN);
   gpio_pull_up(MORSE_KEY_GPIO_PIN);
 
+  // Initialize the LED
+  gpio_init(LED_GPIO_PIN);
+  gpio_set_dir(LED_GPIO_PIN, GPIO_OUT);
+  gpio_put(LED_GPIO_PIN, 1);
+
+  printf("MCP4725_PICO : Init success\r\n");
   while (1) {
     // Check if the morse code key is closed
     if (gpio_get(MORSE_KEY_GPIO_PIN) == 0) {
